1aef1cce9b1bf69a0ed3b0a9be6ef892914118b2
tai <dinhtai0293@gmail.com>
2022-06-21 18:00:01 +0700

    up

--- main.js
+++ main.js
@@ -57,8 +57,11 @@ title[9].onclick = function () {
 
 var myArray = []
 document.querySelector('.top button').onclick = function () {
-    var soN = Number(document.querySelector('.top input').value)
-    myArray.push(' ' + soN)
+    var soN = document.querySelector('.top input').value
+    if (soN == '') { } else {
+        myArray.push(' ' + Number(soN))
+        document.querySelector('.top input').value = ''
+    }
     document.querySelector('.top .soThem').innerHTML = myArray
 }
 
@@ -66,10 +69,15 @@ document.querySelector('.top button').onclick = function () {
 //bài 1: Tổng các số dương trong mảng.
 document.getElementById('btn_bai1').onclick = function () {
     var kqBai1 = 0;
-    for (i = 0; i < myArray.length; i++) {
-        if (myArray[i] > 0) {
-            kqBai1 += Number(myArray[i])
+    if (myArray == '') {
+        kqBai1 = 'Mảng rỗng'
+    } else {
+        for (i = 0; i < myArray.length; i++) {
+            if (myArray[i] > 0) {
+                kqBai1 += Number(myArray[i])
+            }
         }
+        kqBai1 = 'Tổng các số dương bằng: ' + kqBai1
     }
     document.querySelector('.main .kqBai1').innerHTML = kqBai1
 }
@@ -77,10 +85,15 @@ document.getElementById('btn_bai1').onclick = function () {
 // Bài 2: Đếm có bao nhiêu số dương trong mảng.
 document.getElementById('btn_bai2').onclick = function () {
     var kqBai2 = 0;
-    for (i = 0; i < myArray.length; i++) {
-        if (myArray[i] > 0) {
-            kqBai2++
+    if (myArray == '') {
+        kqBai2 = 'Mảng rỗng'
+    } else {
+        for (i = 0; i < myArray.length; i++) {
+            if (myArray[i] > 0) {
+                kqBai2++
+            }
         }
+        kqBai2 = 'Số số dương trong mảng là: ' + kqBai2
     }
     document.querySelector('.main .kqBai2').innerHTML = kqBai2
 }
@@ -88,17 +101,51 @@ document.getElementById('btn_bai2').onclick = function () {
 //bài 3: Tìm số nhỏ nhất trong mảng.
 document.getElementById('btn_bai3').onclick = function () {
     var kqBai3 = 0;
-    for (i = 0; i < myArray.length; i++) {
-        var checkSNN = true
-        for (j = 0; j < myArray.length; j++) {
-            if (myArray[i] - myArray[j] > 0) {
-                checkSNN = false
+    if (myArray == '') {
+        kqBai3 = 'Mảng rỗng'
+    } else {
+        for (i = 0; i < myArray.length; i++) {
+            var checkSNN = true
+            for (j = 0; j < myArray.length; j++) {
+                if (myArray[i] - myArray[j] > 0) {
+                    checkSNN = false
+                }
+            }
+            if (checkSNN) {
+                kqBai3 = myArray[i]
+                break;
             }
         }
-        if (checkSNN) {
-            kqBai3 = myArray[i]
-            break;
-        }
+        kqBai3 = 'Số nhỏ nhất là: ' + kqBai3
     }
     document.querySelector('.main .kqBai3').innerHTML = kqBai3
+}
+
+// Bài 4: Tìm số dương nhỏ nhất trong mảng.
+document.getElementById('btn_bai4').onclick = function () {
+    var kqBai4 = 0;
+    if (myArray == '') {
+        kqBai4 = 'Mảng rỗng'
+    } else {
+        var myArray1 = []
+        for (i = 1; i < myArray.length; i++) {
+            if (myArray[i] > 0) {
+                myArray1.push(myArray[i])
+            }
+        }
+        for (i = 0; i < myArray1.length; i++) {
+            var checkSNN = true
+            for (j = 0; j < myArray1.length; j++) {
+                if (myArray1[i] - myArray1[j] > 0) {
+                    checkSNN = false
+                }
+            }
+            if (checkSNN) {
+                kqBai4 = myArray1[i]
+                break;
+            }
+        }
+        kqBai4 = 'Số dương nhỏ nhất là: ' + kqBai4
+    }
+    document.querySelector('.main .kqBai4').innerHTML = kqBai4
 }
